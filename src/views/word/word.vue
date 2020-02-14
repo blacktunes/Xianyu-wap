35b89a3decd84865170ef4203d205bbb67b29f10
Joshua-hxf <57436673+Joshua-hxf@users.noreply.github.com>
2020-02-14 20:36:19 +0800

    Merge pull request (#2)

---
+++ src/views/word/word.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <h1 class="xyword-title">{{this.page.wordtitle}}</h1>
+    <div class="subcolumn" v-html='text'>{{page.time}}  
+        <i >&icon</i> 
+    {{page.viewer}}</div>
+    <div class="xycontent">{{page.content}}</div>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+export default {
+  max: 500,
+  name: 'Xyword',
+  props: {
+      page: {}
+  },
+  data () {
+    return {
+    }
+  },
+  props: {
+    // view: Array
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import '~@/assets/style/mixins.styl'
+.xyword
+  display flex
+</style>
